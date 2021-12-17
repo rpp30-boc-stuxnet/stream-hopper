5094cb6fda933b9bc5f80386f1e7d86e2154f8b1
Drew Payton <payton.drew@gmail.com>
2021-12-16 21:38:54 -0700

    Reverted changes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000
@@ -8,8 +9,11 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.get('/', cors(), ((req, res) => {
-  res.send('This is working')
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // res.send('This is working')
 }))
 
 app.listen(port, () => {
