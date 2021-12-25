04020552caf8ad3a562f644e0b2acb7d5d31b1c7
Anna <annamp727@gmail.com>
2021-12-24 19:14:05 -0600

    Made user dashboard and wildcard route on server, installed react router

---
+++ client/src/components/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyMovies from './MyMovies/MyMovies.jsx';
+export default function Dashboard() {
+  return (
+    <>
+      <h1>User Dashboard</h1>
+      <MyMovies />
+    </>
+  )
+}
---
+++ client/src/components/MyMovies/MyMovies.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export default function MyMovies() {
+
+  return (
+    <h1>My Movies</h1>
+  )
+}
