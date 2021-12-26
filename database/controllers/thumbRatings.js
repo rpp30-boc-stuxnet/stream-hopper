de30b8b3cff934b8d8c1793de815a34ed7dd4028
Drew Payton <payton.drew@gmail.com>
2021-12-25 19:37:06 -0500

    Added saveThumbRating controller and POST route

--- database/controllers/thumbRatings.js
+++ database/controllers/thumbRatings.js
@@ -5,7 +5,6 @@ const Overall_Thumb_Rating = require('../models/overallThumbRating.js');
 const findThumbRatings = async (req, res) => {
   await User_Thumb_Rating.findOne({user_id: req.query.user_id, tmdb_id: req.query.tmdb_id})
     .then(async (userThumbRating) => {
-      console.dir(userThumbRating);
       let user_thumb_rating = userThumbRating.thumb_rating || null;
       await Overall_Thumb_Rating.findOne({tmdb_id: req.query.tmdb_id})
         .then((overallThumbRating) => {
@@ -26,9 +25,82 @@ const findThumbRatings = async (req, res) => {
 }
 
 const saveThumbRating = async (req, res) => {
-  // req.body: {tmdb_id, user_id, prevThumbRating, newThumbRating}
-    // prevThumbRating and newThumbRating can be null, 'up', or 'down'
-  console.log('inside saveThumbRating')
+  // req.body: {tmdb_id, user_id, prev_thumb_rating, new_thumb_rating}
+    // prev_thumb_rating and new_thumb_rating can be null, 'up', or 'down'
+
+  if (req.body.prev_thumb_rating === req.body.new_thumb_rating) {
+    res.status(200).send('No changes made.  Previous and new thumb ratings were the same');
+    return;
+  }
+
+  User_Thumb_Rating.findOneAndUpdate(
+    { user_id: req.body.user_id, tmdb_id: req.body.tmdb_id },
+    { thumb_rating: req.body.new_thumb_rating },
+    {new: true, upsert: true, useFindAndModify: false },
+    async (error, response) => {
+      if (error) {
+        res.status(400).send('Error during findOneAndUpdate User_Thumb_Rating: ' + error);
+        return;
+      } else {
+        // findOne to see if movie has thumb ratings
+        await Overall_Thumb_Rating.find({ tmdb_id: req.body.tmdb_id })
+          .then(async (results) => {
+            // if movie doesn't have any existing thumb ratings
+            if (results.length === 0) {
+              let thumbsUps = 0;
+              let thumbsDowns = 0;
+
+              if (req.body.new_thumb_rating === 'up') {
+                thumbsUps = 1;
+              } else if (req.body.new_thumb_rating === 'down') {
+                thumbsDowns = 1;
+              }
+
+              await Overall_Thumb_Rating.create({tmdb_id: req.body.tmdb_id, thumbs_ups: thumbsUps, thumbs_downs: thumbsDowns})
+                .then(() => {
+                  res.status(201).send('Thumb rating saved successfully');
+                  return;
+                })
+                .catch((error) => {
+                  res.status(400).send(error)
+                  return;
+                })
+            } else {
+              // update existing one
+              let thumbsUpChange = 0;
+              let thumbsDownChange = 0;
+
+              if (req.body.prev_thumb_rating === 'up') {
+                thumbsUpChange += -1;
+              } else if (req.body.prev_thumb_rating === 'down') {
+                thumbsDownChange += -1;
+              }
+
+              if (req.body.new_thumb_rating === 'up') {
+                thumbsUpChange += 1;
+              } else if (req.body.new_thumb_rating === 'down') {
+                thumbsDownChange += 1;
+              }
+
+              results[0].updateOne(
+                { $inc: {'thumbs_ups': thumbsUpChange, 'thumbs_downs': thumbsDownChange } },
+                // {new: true, upsert: true, useFindAndModify: false },
+                async (error, response) => {
+                  if (error) {
+                    res.status(400).send('Error during findOneAndUpdate User_Thumb_Rating: ' + error);
+                    return;
+                  } else {
+                    res.status(201).send('Thumb rating saved successfully');
+                    return;
+                  }
+                }
+              )
+            }
+          })
+      }
+    }
+  )
+
 }
 
 // const addSavedTitle = async (req, res) => {
