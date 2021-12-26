72ed7d02c9c1ac0587529672c1d61f749d228169
Ashley Reischman <ashleyreischman648@gmail.com>
2021-12-26 13:02:30 +0200

    added logic for google sign in - it works and adds the unique user id to session storage. sesson storage automatically clears when the broswer tab/window closes

--- client/src/components/LoginOrSignup.jsx
+++ client/src/components/LoginOrSignup.jsx
@@ -1,6 +1,42 @@
 import React from 'react';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+import { getAuth, signInWithPopup, GoogleAuthProvider }  from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
 
 const LoginOrSignup = (props) => {
+
+  const googleLogin = () => {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyBa8Qq6QRdsUIdcCGbFGdCwjlMKHesWanI",
+      authDomain: "stuxnet-boc-rpp30.firebaseapp.com",
+      projectId: "stuxnet-boc-rpp30",
+      storageBucket: "stuxnet-boc-rpp30.appspot.com",
+      messagingSenderId: "916683907756",
+      appId: "1:916683907756:web:64912631b6e6d77105f509",
+      measurementId: "G-64RFV7ZW0E"
+    };
+
+    initializeApp(firebaseConfig);
+    const auth = getAuth();
+    let provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        window.sessionStorage.setItem('userUID',result.user.uid);
+        //send the user up to the server
+
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        //need to handle errors/inform the user of errors
+      });
+  }
 
   return (
     <div>
@@ -8,17 +44,17 @@ const LoginOrSignup = (props) => {
       <div>
         <button onClick={props.handleXOutClick}>X</button>
       </div>
-      <button>Google</button>
+      <button onClick={googleLogin}>Google</button>
       <button>Facebook</button>
       <p>-OR-</p>
       <div>
         <form>
           <div>
-            <label for='username'>Username</label>
+            <label htmlFor='username'>Username</label>
             <input type='text' name='username' placeholder='Enter Username (max 50 chars)'/>
           </div>
           <div>
-            <label for='password'>Password</label>
+            <label htmlFor='password'>Password</label>
             <input type='text' name='password' placeholder='Enter password'/>
           </div>
         </form>
