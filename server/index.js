3d195fe834fa5bcadcf6495b14b21762ec4df3af
Ashley Reischman <ashleyreischman648@gmail.com>
2021-12-27 13:50:02 +0200

    added logic to run an https server if needed for facebook testing

--- server/index.js
+++ server/index.js
@@ -10,30 +10,50 @@ const savedTitlesRoutes = require('./routes/savedTitlesRoutes.js');
 const relatedTitlesRoutes = require('./routes/relatedTitlesRoutes.js');
 const titleDetailsRoutes = require('./routes/titleDetailsRoutes.js');
 
-connectDB();
 
-const app = express();
+//FOR FACEBOOK TESTING ONLY BECAUSE FACEBOOK LOGIN DOES NOT ACCEPT HTTP REQUESTS.
+if (process.env.ENABLE_HTTPS_SERVER === "active") {
+  const fs = require('fs');
+  const https = require('https');
+  const privateKey  = fs.readFileSync('/Users/ashleyreischman/HackReactor/blueOcean/stream-hopper/localhost-key.pem', 'utf8');
+  const certificate = fs.readFileSync('/Users/ashleyreischman/HackReactor/blueOcean/stream-hopper/localhost.pem', 'utf8');
+  const credentials = {key: privateKey, cert: certificate};
+  const app = express();
 
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-app.use(cors());
-app.use('/', express.static(path.join(__dirname, '../client/build')));
-app.use('/homepage',express.static(path.join(__dirname, '../client/build')))
+  app.use('/', express.static(path.join(__dirname, '../client/build')));
+  app.use('/homepage',express.static(path.join(__dirname, '../client/build')))
 
-app.use('/testDbData', testRoutes);
-app.use('/savedTitles', savedTitlesRoutes);
-app.use('/relatedTitles', relatedTitlesRoutes);
-app.use('/titleDetails', titleDetailsRoutes);
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(8443);
+  console.log('running https server on 8443')
+} else {
 
-app.get('/', cors(), ((req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-}))
+  connectDB();
 
-app.get('/testroute', cors(), ((req, res) => {
-  res.send('Successful GET call to /testroute express route');
-}))
+  const app = express();
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  app.use(express.urlencoded({extended:true}));
+  app.use(express.json());
+  app.use(cors());
+  app.use('/', express.static(path.join(__dirname, '../client/build')));
+  app.use('/homepage',express.static(path.join(__dirname, '../client/build')))
+
+  app.use('/testDbData', testRoutes);
+  app.use('/savedTitles', savedTitlesRoutes);
+  app.use('/relatedTitles', relatedTitlesRoutes);
+  app.use('/titleDetails', titleDetailsRoutes);
+
+  app.get('/', cors(), ((req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  }))
+
+  app.get('/testroute', cors(), ((req, res) => {
+    res.send('Successful GET call to /testroute express route');
+  }))
+
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+  })
+}
+
 
