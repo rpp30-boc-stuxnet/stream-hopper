80bd27f37ebc0945779a8d8795825c55241d4a25
Trevor <trevorhackreactor@Gmail.com>
2022-01-03 19:57:21 -0500

    more functionality to search bar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 const axios = require('axios')
 
 
@@ -9,6 +10,13 @@ class SearchBar extends React.Component {
       searchData: [],
     }
     this.searchChange = this.searchChange.bind(this);
+    this.searchEnter = this.searchEnter.bind(this);
+  }
+
+  searchEnter = (e) => {
+    if (e.key === 'Enter') {
+      console.log('ENTER WAS PRESSED')
+    }
   }
 
   searchChange = (e) => {
@@ -18,7 +26,7 @@ class SearchBar extends React.Component {
       }
     })
     .then((res) => {
-      // console.log(res)
+      console.log(res.data)
       this.setState({searchData: res.data})
     })
     .catch((err) => {
@@ -35,21 +43,26 @@ class SearchBar extends React.Component {
             placeholder="search for a movie or tv show"
             value={this.state.searchTerm}
             onChange={(event) => this.searchChange(event)}
+            onKeyPress={(event) => this.searchEnter(event)}
           />
         </div>
+         {this.state.searchData.length > 0 && (
         <div className="dataResults">
           {this.state.searchData.map((data, index) => {
             return (
               <div key={index}>
+                <Link to={`/movieDetails/${data.tmdb_id}`}>
                 <div style={{display:'inline'}}>
-                <img src={data.poster_path} alt='movie poster' width="40px" height="40px"></img>
+                <img src={data.poster_path !== 'https://image.tmdb.org/t/p/w500null' ? data.poster_path : "https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg"} alt='movie poster' width="40px" height="40px"></img>
                 <span style={{fontSize: "10px", display:"block",color:"black"}}>{data.release_date}</span>
-                <span style={{fontSize: "10px", display:"block",color:"black"}}>{data.title}</span>
-                </div>
+                <span style={{fontSize: "10px", display:"block"   ,color:"black"}}>{data.title}</span>
               </div>
+              </Link>
+            </div>
             )
           })}
         </div>
+         )}
       </div>
     )
   }
