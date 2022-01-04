9baf694eb75f5577559eecb3ff20f0da2083ea3b
Drew Payton <payton.drew@gmail.com>
2022-01-03 17:56:17 -0700

    Fixed bug where search results were returning broken poster url's

--- server/routes/searchRoutes.js
+++ server/routes/searchRoutes.js
@@ -17,12 +17,18 @@ router.get("/", async (req, res) => {
         for (var i = 0; i < response.data.results.length; i++) {
           if (response.data.results[i].media_type === 'tv' || response.data.results[i].media_type === 'movie') {
             let individualResult = response.data.results[i];
+            let posterURL;
+            if (individualResult.poster_path) {
+              posterURL = 'https://image.tmdb.org/t/p/w500' + individualResult.poster_path
+            } else {
+              posterURL = 'https://i.imgur.com/7sR45d6.png'
+            }
             transformedResults.push({
               tmdb_id: individualResult.id,
               type: individualResult.media_type,
               title: individualResult.title || individualResult.name,
               release_date: individualResult.release_date || individualResult.first_air_date,
-              poster_path: 'https://image.tmdb.org/t/p/w500' + individualResult.poster_path
+              poster_path: posterURL
             })
           }
         }
