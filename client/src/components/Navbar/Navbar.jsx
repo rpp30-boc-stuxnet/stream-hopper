96525686c8c0d37f2cfbccf9be18977177882678
Raine Adams <rainedoescode@gmail.com>
2022-01-07 23:40:29 -0600

    Added link to homepage in navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar.jsx';
 
 // navbar, currently assumes user is logged in
 export default function Navbar(props) {
 
   return (
-    <nav className={'navbar'} >
-      <span className={'navLogo'} >streamhopper!</span>
-      {/** Search bar will go here */}
+    <nav className='navbar' >
+      <Link to={'/homepage'}>
+        <span className='navLogo' >streamhopper!</span>
+      </Link>
       <SearchBar />
+      <Link to={'/homepage'}>
+        <span className='homeLink' >homepage</span>
+      </Link>
       <button
-        className={'logoutBtn'}
+        className='logoutBtn'
         onClick={props.handleLogout} >
           Logout
       </button>
