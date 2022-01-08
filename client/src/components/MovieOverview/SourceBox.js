4c192c8c32feda08aef6b9f0416c4ce95fd245c7
ErnestoOrtegaHernandez <ernesto.ortega53@gmail.com>
2022-01-08 11:24:32 -0500

    Added "fill out fields" messages to the rating form

--- client/src/components/MovieOverview/SourceBox.js
+++ client/src/components/MovieOverview/SourceBox.js
@@ -1,20 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import './SourceBox.css';
 import SourceReview from './SourceReview.js';
-
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 function SourceBox(props){
 
+  let params = useParams();
+  let mediaId = params.id;
+  let mediaType = params.type;
 
   const [showForm, setShowForm] = useState(false);
+  const [streamHopperQuality, setStreamHopperQuality] = useState(undefined);
 
   const handleToggle = (e) =>{
     e.preventDefault();
     setShowForm(!showForm);
   }
+  // useEffect(()=>{
+  //   let options = {
+  //     user_id: window.localStorage.userUID,
+  //       tmdb_id: mediaId,
+  //       source_company_id: props.companyId,
+  //       stream_type: mediaType,
+  //       stream_format: props.quality
+  //   }
+  //   console.log(options, 'options')
+  //   axios.get('/api/streamSources', {params: options})
+  //   .then((response)=>{
+  //     console.log(response, 'getStreamQuality')
+  //   })
+  //   .catch((error)=>{
+  //     return error
+  //   })
+  // }, [])
 
   return (
-    <div className = "sourceBox">
+    <div className = "sourceBox ">
       <div className ="sourceBoxImageContainer">
         {props.logoURL ?
           <img src = {props.logoURL} className = "sourceBoxImage" onClick = {handleToggle}/>
@@ -26,7 +48,8 @@ function SourceBox(props){
       <div className = "sourceBoxInternalRating">
       {(props.price && props.quality) ? '100%' : 'Quality Not Available'}
       </div>
-      {showForm ? <SourceReview handleToggle = {handleToggle} quality = {props.quality} companyId = {props.companyId}/> : null}
+      {showForm ? <SourceReview handleToggle = {handleToggle} quality = {props.quality} companyId = {props.companyId}
+      titleName = {props.titleName} companyName = {props.companyName}/> : null}
     </div>
   )
 }
