d2c3823131c4a395429c5c818ca80d8cf195e040
Drew Payton <payton.drew@gmail.com>
2022-01-10 16:07:19 -0700

    Refactored server to separate app from index.  Created first jest/supertest test and verified it is working correctly.  Added npm run back-test script to package.json and integration.yml files

--- database/index.js
+++ database/index.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
+const connect = async () => {
+  let connectionString;
 
+  if (process.env.NODE_ENV === 'test') {
+    console.log('Using MongoDB Test Environment 🔬');
+    connectionString = process.env.MONGO_TEST_URI;
+  } else {
+    connectionString = process.env.MONGO_URI;
+  }
+
+  try {
+    await mongoose.connect(connectionString);
     console.log("MongoDB Connection Success 👍");
   } catch (error) {
     console.log("MongoDB Connection Failed 💥");
@@ -13,4 +21,9 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+const disconnect = async () => {
+  await mongoose.connection.close()
+}
+
+exports.connect = connect;
+exports.disconnect = disconnect;
--- server/index.js
+++ server/index.js
@@ -1,72 +1,9 @@
-require('dotenv').config();
-const path = require('path');
-const express = require('express');
+const app = require('./app')
 const port = process.env.PORT || 4000
-const connectDB = require("../database/index.js");
-const cors = require('cors');
+const database = require("../database/index.js");
 
-const testRoutes = require('./routes/testRoutes.js');
-const savedTitlesRoutes = require('./routes/savedTitlesRoutes.js');
-const relatedTitlesRoutes = require('./routes/relatedTitlesRoutes.js');
-const titleDetailsRoutes = require('./routes/titleDetailsRoutes.js');
-const searchRoutes = require('./routes/searchRoutes.js');
-const thumbRatings = require('./routes/thumbRatings.js');
-const streamSources = require('./routes/streamSources.js');
-const streamRatings = require('./routes/streamRatings.js');
-const trendingTitles = require('./routes/trendingTitlesRoutes.js');
-const spielbergTitles = require('./routes/spielbergTitlesRoutes.js');
-const titleReviews = require('./routes/titleReviewsRoutes.js');
+database.connect();
 
-
-//FOR FACEBOOK TESTING ONLY BECAUSE FACEBOOK LOGIN DOES NOT ACCEPT HTTP REQUESTS//
-//we should get rid of this once we're confident in the FB login
-if (process.env.ENABLE_HTTPS_SERVER === "active") {
-  const fs = require('fs');
-  const https = require('https');
-  const privateKey  = fs.readFileSync('/Users/ashleyreischman/HackReactor/blueOcean/stream-hopper/localhost-key.pem', 'utf8');
-  const certificate = fs.readFileSync('/Users/ashleyreischman/HackReactor/blueOcean/stream-hopper/localhost.pem', 'utf8');
-  const credentials = {key: privateKey, cert: certificate};
-  const app = express();
-
-  app.use('/', express.static(path.join(__dirname, '../client/build')));
-  app.use('/homepage',express.static(path.join(__dirname, '../client/build')))
-
-  const httpsServer = https.createServer(credentials, app);
-  httpsServer.listen(8443);
-  console.log('running https server on 8443')
-} else {
-  const app = express();
-  app.use(express.urlencoded({extended:true}));
-  app.use(express.json());
-  app.use(cors());
-  app.use('/', express.static(path.join(__dirname, '../client/build')));
-  app.use('/homepage',express.static(path.join(__dirname, '../client/build')))
-
-  app.use('/api/testDbData', testRoutes);
-  app.use('/api/savedTitles', savedTitlesRoutes);
-  app.use('/api/relatedTitles', relatedTitlesRoutes);
-  app.use('/api/titleDetails', titleDetailsRoutes);
-  app.use('/api/search', searchRoutes);
-  app.use('/api/thumbRatings', thumbRatings);
-  app.use('/api/streamSources', streamSources);
-  app.use('/api/streamRatings', streamRatings);
-  app.use('/api/trendingTitles', trendingTitles);
-  app.use('/api/spielbergTitles', spielbergTitles);
-  app.use('/api/titleReviews', titleReviews);
-
-  connectDB();
-
-  app.get('/', cors(), ((req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  }))
-
-  app.get('/testroute', cors(), ((req, res) => {
-    res.send('Successful GET call to /testroute express route');
-  }))
-
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-  })
-}
-
-
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
