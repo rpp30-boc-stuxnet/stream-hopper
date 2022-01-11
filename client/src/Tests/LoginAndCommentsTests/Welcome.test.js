6ac79f866995f4289fcbd94abf9201179d15e384
Ashley Reischman <ashleyreischman648@gmail.com>
2022-01-10 21:38:15 -0800

    added test for checking functionality of clicking the 'Log in' button

--- client/src/Tests/LoginAndCommentsTests/Welcome.test.js
+++ client/src/Tests/LoginAndCommentsTests/Welcome.test.js
@@ -6,6 +6,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 
 //component
 import Welcome from '../../components/LoginComponents/Welcome.jsx';
@@ -14,6 +15,12 @@ describe ('Welcome Component Test', () => {
   test('Should be on the DOM', function () {
     render (<Welcome />)
     expect(screen.getByText('Welcome to Streamhopper')).toBeInTheDocument();
+  })
+
+  test('Should render login options when the user clicks `Login`', () => {
+    render(<Welcome />)
+    userEvent.click(screen.getByText('Log in'));
+    expect(screen.getByText('Log in with Google')).toBeInTheDocument();
   })
   //test for pushing login button and having the correct words show up, and same for signup.
 })
