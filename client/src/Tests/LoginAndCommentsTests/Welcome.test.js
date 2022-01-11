6fd8ff9bd029bc33d25f575d508f39cdcedf1a14
Ashley Reischman <ashleyreischman648@gmail.com>
2022-01-10 22:09:13 -0800

    finished up tests for welcome page/login and sign up interactions that do not involve FB or Google.

--- client/src/Tests/LoginAndCommentsTests/Welcome.test.js
+++ client/src/Tests/LoginAndCommentsTests/Welcome.test.js
@@ -4,7 +4,7 @@
 
 //dependencies
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
 
@@ -36,6 +36,29 @@ describe ('Welcome Component Test', () => {
 
     userEvent.click(screen.getByText('X'));
     expect(screen.getByText('Welcome to Streamhopper')).toBeInTheDocument();
+  })
+
+  test('Entering an invalid email causes the email warning to render and the log in button to be disabled', () => {
+    render(<Welcome />)
+
+    userEvent.click(screen.getByText('Sign up'));
+    const input = screen.getByTestId('test-userEmailInput');
+    fireEvent.change(input, {target: {value: 'ashley'}});
+    expect(screen.getByText('Please enter a valid email address')).toBeInTheDocument();
+    expect(screen.getByTestId('test-submitBtn')).toBeDisabled();
+  })
+
+  test('clicking on the `show password` should chnage the type of the password box to `text`. Clicking it again should change it back.', () => {
+    render(<Welcome />)
+
+    userEvent.click(screen.getByText('Sign up'));
+
+    const passwordInput = screen.getByTestId('test-passwordEntry');
+    fireEvent.change(passwordInput, {target: {value: 'hello'}});
+    userEvent.click(screen.getByText('Show Password'));
+    expect(screen.getByTestId('test-passwordEntry')).toHaveAttribute('type','text');
+    userEvent.click(screen.getByText('Hide Password'));
+    expect(screen.getByTestId('test-passwordEntry')).toHaveAttribute('type','password');
   })
 
 
