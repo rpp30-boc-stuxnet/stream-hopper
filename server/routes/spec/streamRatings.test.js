9900e155ee9ea11b9ec3099772890128a799d433
Drew Payton <payton.drew@gmail.com>
2022-01-12 13:23:59 -0700

    Wrote skeleton for /api/streamRatings tests.  Removed coverage reports repo

---
+++ server/routes/spec/streamRatings.test.js
@@ -0,0 +1,87 @@
+const request = require('supertest');
+const app = require('../../app.js');
+const database = require("../../../database/index.js");
+
+beforeAll(async () => {
+  await database.connect();
+  await database.drop();
+})
+
+afterAll(async () => {
+  await database.disconnect();
+})
+
+describe("GET /api/streamRatings", () => {
+  describe("When a required query paramater is missing from request", () => {
+    test("Should respond with a 400 status code", async () => {
+
+    })
+
+    test("Should responsd with a message indicating the parameter that was missing", async () => {
+
+    })
+
+  })
+
+  describe("When a user has not rated a stream source, but others have", () => {
+    test("Should respond with a 200 status code", async () => {
+
+    })
+
+    test("Should respond with an object that contains all of the expected properties", async () => {
+
+    })
+
+    test("Should return 'null' for each of the user's ratings", async () => {
+
+    })
+
+    test("Should return numeric values for each of the overall average ratings", async () => {
+
+    })
+
+    test("Should return at least 1 for the 'count_of_reviews' property", async () => {
+
+    })
+
+  })
+
+  describe("When no users have rated a stream source", () => {
+    test("Should respond with a 200 status code", async () => {
+
+    })
+
+    test("Should respond with an object that contains all of the expected properties", async () => {
+
+    })
+
+    test("Should return 'null' for all of the specific rating fields", async () => {
+
+    })
+
+    test("Should return 0 for the 'count_of_reviews' property", async () => {
+
+    })
+
+  })
+
+  describe("When a user has rated a stream source", () => {
+    test("Should respond with a 200 status code", async () => {
+
+    })
+
+    test("Should respond with an object that contains all of the expected properties", async () => {
+
+    })
+
+    test("Should return numeric values for all of the specific rating fields", async () => {
+
+    })
+
+    test("Should return at least 1 for the 'count_of_reviews' property", async () => {
+
+    })
+
+  })
+
+})
